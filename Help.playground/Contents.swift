733c448664cec9396708b427427ef236021d7707
pressstart101 <walterscott404@yandex.com>
2016-07-05 18:14:43 -0400

    brains

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,7 @@ name.uppercaseString
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+let say = "brains"
+let zombiesays = say.uppercaseString
+print("im a zombie i need \(zombiesays)")
